d180a3d285bfcf110e26d0fa62e71b4ac5c90cac
Prerna Aneja <57767359+anejaprerna19@users.noreply.github.com>
2021-09-05 16:22:49 +0530

    Add files via upload
    Talks about findamental data types (int and float) and some operators like +,-,*,**,/,//,% etc.

---
+++ fundp.py
@@ -0,0 +1,9 @@
+#We will talk about fundamental data types
+#Here, we will talk about int and float
+print(type(2+4))
+print(type(2-4))
+print(type(2/4)) #0.5, thus prints <class'float'>
+print(type(9.9+1.1)) #float 
+print(2 ** 4) #2^4=16
+print(5 // 4) #divides but returns an integer
+print( 3%4)  #Returns the remainder
