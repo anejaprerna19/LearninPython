79b5c6595ca30c43b1d3fdb8aca039af1fc62dd8
Prerna Aneja <57767359+anejaprerna19@users.noreply.github.com>
2021-09-12 14:12:15 +0530

    Add files via upload
    Introducing Strings.
    In this program, we will see how to use strings.

---
+++ strings_intro.py
@@ -0,0 +1,12 @@
+#Working with strings
+#In this program, we will see how to use strings. 
+name= input("What is your name? ");
+age= input("What is your age? ");
+print('Hi '+ name + ', You are ' + age + ' years old')
+#String formatting is used which is another way to print strings. Both serve the same purpose
+print(f'Hi {name}, You are {age} years old');
+#Console :
+#What is your name? Rekhi
+#What is your age? 50
+#Hi Rekhi, You are 50 years old
+#Hi Rekhi, You are 50 years old
